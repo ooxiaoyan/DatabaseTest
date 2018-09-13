be75917ce3fee8f4f7a20a01d74fc9c61d81c8a3
ooxiaoyan <810978527@qq.com>
2018-09-13 15:39:48 +0800

    Create data in database

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();//create/open a database
+            }
+        });
+
+        Button addData = findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values); // 插入第一条数据
+                values.clear();
+                // 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); // 插入第二条数据
             }
         });
     }
